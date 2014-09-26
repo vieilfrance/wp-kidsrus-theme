c5a50660dbb7f25fe07ac652ba4da42b2e4041d7
David Beck <dbec.pro@gmail.com>
2014-09-26 22:35:19 +0200

    setup footer

--- footer.php
+++ footer.php
@@ -10,16 +10,49 @@
  * @since Kidsrus 1.0
  */
 ?>
-	
-	<footer class="grey-footer">
-		<div class="container">
+	<footer class="">
+		<div class="container invert-footer">
 			<div class="row footer-center">
-				<div>&copy;2014 by David BECK</div>
+				<div class="col-xs-12 col-sm-5">
+					<div>Adresse crêche montessori dupe
+					blablabla adresse
+					blabla zipcode
+					<a href="#">Google maps</a>
+					</div>
+					<div>Téléphone crêche montessori dupe
+					+32 034355454
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-5">
+					<div><a href="#">Terms &amp; Conditions</a></div>
+					<div><a href="#">Crédits</a></div>
+					<div>contact@xxx.be</div>
+				</div>
+				<div class="col-xs-12 col-sm-2">
+				  <a href="http://www.facebook.com/#" title="facebook"><i class="fa fa-facebook-square fa-2x"></i></a>
+				  <a href="http://www.twitter.com/#" title="twitter"><i class="fa fa-twitter-square fa-2x"></i></a>
+				  <a href="http://www.instagram.com/#" title="instagram"><i class="fa fa-instagram fa-2x"></i></a>
+	   		    </div>
+			</div>
+		</div>
+	</footer>
+
+	<footer class="">
+		<div class="container grey-footer">
+			<div class="row footer-center">
+	   		    <div class="col-xs-12 col-sm-4">
+	   		    </div>
+	   		    <div class="col-xs-12 col-sm-4">
+				<div>Copyright &copy;2014 Dupe Akiniyi</div>
+				<div>Site by <a href="#">David BECK</a></div>
+	   		    </div>
+	   		    <div class="col-xs-12 col-sm-4">
+	   		    </div>
 			</div>
 		</div>
 	</footer>
 </div>
-<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto:300,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
 </body>
